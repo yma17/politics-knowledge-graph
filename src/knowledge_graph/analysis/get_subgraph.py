5aeef7efe3a4d6fd997d9ee882d7d9a188683813
Eric Ma <ericma@comcast.net>
2022-11-20 22:49:14 -0500

    Fix bug in get_subgraph, example q1

--- src/knowledge_graph/analysis/get_subgraph.py
+++ src/knowledge_graph/analysis/get_subgraph.py
@@ -5,31 +5,12 @@ given a set of edge types able to be traversed.
 """
 
 import dgl
-from collections import defaultdict
 
 def get_subgraph(g, start_node_ids, valid_edge_types):
     # Filter by edge type
     sg = dgl.edge_type_subgraph(g, valid_edge_types)
     # Filter by start nodes
     sg = dgl.out_subgraph(sg, start_node_ids)
-    # Remove nodes with no incident edges
-    s2spo, o2spo = defaultdict(lambda: []), defaultdict(lambda: [])
-    for s, p, o in valid_edge_types:
-        s2spo[s].append((s, p, o))
-        o2spo[o].append((s, p, o))
-    for ntype in sg.ntypes:
-        nodes_to_remove = []
-        for node in sg.nodes(ntype):
-            remove = True
-            if ntype in s2spo.keys():
-                for spo in s2spo[ntype]:
-                    remove = remove and (sg.successors(node, etype=spo).size()[0] == 0)
-            if ntype in o2spo.keys():
-                for spo in o2spo[ntype]:
-                    remove = remove and (sg.predecessors(node, etype=spo).size()[0] == 0)
-            if remove:
-                nodes_to_remove.append(node)
-        sg.remove_nodes(nodes_to_remove, ntype=ntype)
     return sg
 
 if __name__ == '__main__':
--- src/knowledge_graph/analysis/q1_party_distribution.py
+++ src/knowledge_graph/analysis/q1_party_distribution.py
@@ -1,1 +1,24 @@
 """Q1: What is the distribution of political parties?"""
+
+from get_subgraph import get_subgraph
+import dgl
+import pandas as pd
+from collections import Counter
+
+(g,), _ = dgl.load_graphs('../../../data/graph.dgl')
+start_node_ids = {'member': [10,11,12,13,14]}  # example only
+valid_edge_types = [
+    ('member', 'memberof', 'party')]
+sg = get_subgraph(g, start_node_ids, valid_edge_types)
+member_srcids, party_tgtids = sg.edges(etype=('member', 'memberof', 'party'))
+#print(member_srcids)
+#print(party_tgtids)
+
+df_node = pd.read_csv('../../../data/nodes.csv')
+df_node_party = df_node[df_node["ntype"]=='party']
+df_node_party.set_index("nid_type", inplace=True)
+
+#print(df_node_party)
+
+c = Counter([df_node_party.loc[x.item()]["nname"] for x in party_tgtids])
+print(c)
